005e790615720e7a4a3da6c4843fb757b8b39662
hacenbarb <cenha06@gmail.com>
2022-06-21 14:42:33 +0100

    magna website

---
+++ scripts/mainscript.js
@@ -0,0 +1,8 @@
+// $(function () {
+//   "use strict";
+//   var winh = $(window).height();
+//   $("header").height(winh);
+// });
+// let heroHeight = document.querySelector('#hero-content').offsetHeight;
+// let heroImg = document.querySelector('.hero-media');
+// heroImg.style.height = heroHeight + 'px';
